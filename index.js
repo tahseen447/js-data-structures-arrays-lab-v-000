fbe16a453fc391c2a16308f40598c48e0534b03a
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-08 00:34:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,9 @@ function destructivelyAppendDriver (name) {
 }
 
 function destructivelyPrependDriver(name) {
-  drivers.unshift(name)
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
 }
