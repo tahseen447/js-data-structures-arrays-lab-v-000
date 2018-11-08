d70a3a68facb55a86e16a63524bcc429fa9510b8
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-08 00:36:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,7 @@ function destructivelyRemoveLastDriver() {
 function destructivelyRemoveFirstDriver() {
   drivers.shift();
 }
+
+function appendDriver(name) {
+  let newArray = [name, ..drivers];
+}
