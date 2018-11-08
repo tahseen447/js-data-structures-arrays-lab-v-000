dbb17e45ea6b91a14199b1705e46679a5363352a
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-08 00:33:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,9 @@
 const drivers = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendDriver (name) {
-  drivers.push('Ralph');
+  drivers.push(name');
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name)
 }
